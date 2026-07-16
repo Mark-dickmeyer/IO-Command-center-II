63d388f621ff593cc83e9cb4b151f711aec96374
Mark-dickmeyer <mark.dickmeyer@ahead.com>
2026-07-16 17:48:28 -0400

    Initialize storage before rendering the React app
    Refactor main.jsx to initialize storage before rendering the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import "./storageShim.js";
+import { initStorage } from "./storageShim.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function start() {
+  await initStorage();
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+start();
